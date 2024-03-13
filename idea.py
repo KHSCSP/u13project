cfb626db006cf9f1e8da6171c92bdb0eab7d2a21
heyscottn <heyscottn@gmail.com>
2024-03-12 21:04:02 -0500

    mm

---
+++ idea.py
@@ -0,0 +1,18 @@
+options = '''
+\nHere are your options:
+(act) to show all activities
+(solo) to show all solo activities
+(acc) to show all accessible activities
+(cat) to get a category of activities
+-----
+(meme) to show all memes
+(link) to show all memes with a url
+(large) to show all large memes
+(favs) to get all memes of your favorite category
+(q) or (quit) to quit
+'''
+
+choice = 'jjj'
+while choice[0].lower() != 'q':
+    print(options)
+    choice = input()
